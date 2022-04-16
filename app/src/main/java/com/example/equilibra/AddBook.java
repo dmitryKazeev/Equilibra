408ce65910334e599f2df3eb7e93cf628aa612ed
dimak <dimakazeev31@gmail.com>
2022-04-16 18:51:51 +0600

    dfdfd

--- app/src/main/java/com/example/equilibra/AddBook.java
+++ app/src/main/java/com/example/equilibra/AddBook.java
@@ -3,40 +3,64 @@ package com.example.equilibra;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
-public class AddBook extends AppCompatActivity {
+public class AddBook extends AppCompatActivity implements View.OnClickListener{
 
     EditText bookNameTxt, authorNameTxt, conditionTxt, exchangeForTxt;
     Button addBtn;
 
+    MyDBHelper dbHelper;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
+        addBtn = (Button) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener((View.OnClickListener) this);
 
-        bookNameTxt = findViewById(R.id.bookNameTxt);
-        authorNameTxt = findViewById(R.id.authorNameTxt);
-        conditionTxt = findViewById(R.id.conditionTxt);
-        exchangeForTxt = findViewById(R.id.exchangeForTxt);
-        addBtn = findViewById(R.id.addBtn);
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MyDBHelper myDB = new MyDBHelper(AddBook.this);
-                myDB.AddBook(bookNameTxt.getText().toString().trim(),
-                        authorNameTxt.getText().toString().trim(),
-                        conditionTxt.getText().toString().trim(),
-                        exchangeForTxt.getText().toString().trim()
-                        );
-            }
-        });
+
+        bookNameTxt =(EditText) findViewById(R.id.bookNameTxt);
+        authorNameTxt =(EditText) findViewById(R.id.authorNameTxt);
+        conditionTxt =(EditText) findViewById(R.id.conditionTxt);
+        exchangeForTxt =(EditText) findViewById(R.id.exchangeForTxt);
+        dbHelper = new MyDBHelper(this);
 
     }
+    @Override
+    public void onClick(View view) {
+        String bookName = bookNameTxt.getText().toString();
+        String authorName = authorNameTxt.getText().toString();
+        String condition = conditionTxt.getText().toString();
+        String exchangeFor = exchangeForTxt.getText().toString();
+
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
 
 
+        switch (view.getId()){
+
+            case R.id.addBtn:
+                contentValues.put(dbHelper.COLUMN_TITLE , bookName);
+                contentValues.put(dbHelper.COLUMN_AUTHOR , authorName);
+                contentValues.put(dbHelper.COLUMN_CONDITION , condition);
+                contentValues.put(dbHelper.COLUMN_EXCHANGE_FOR , exchangeFor);
+
+                database.insert(dbHelper.TABLE_NAME , null , contentValues);
+
+
+                break;
+        }
+    }
 
 }
--- app/src/main/java/com/example/equilibra/MyDBHelper.java
+++ app/src/main/java/com/example/equilibra/MyDBHelper.java
@@ -12,15 +12,15 @@ import androidx.annotation.Nullable;
 
 public class MyDBHelper extends SQLiteOpenHelper {
 
-    private Context context;
-    private static final String DATABASE_NAME="BookExchange.db";
-    private static final int DATABASE_VERSION= 1;
-    private static final String TABLE_NAME="book_nearby";
-    private static final String COLUMN_ID="_id";
-    private static final String COLUMN_TITLE="book_title";
-    private static final String COLUMN_AUTHOR="author_name";
-    private static final String COLUMN_CONDITION="condition";
-    private static final String COLUMN_EXCHANGE_FOR="exchange_for";
+    public Context context;
+    public static final String DATABASE_NAME="BookExchange.db";
+    public static final int DATABASE_VERSION= 1;
+    public static final String TABLE_NAME="book_nearby";
+    public static final String COLUMN_ID="_id";
+    public static final String COLUMN_TITLE="book_title";
+    public static final String COLUMN_AUTHOR="author_name";
+    public static final String COLUMN_CONDITION="condition";
+    public static final String COLUMN_EXCHANGE_FOR="exchange_for";
 
     public MyDBHelper(@Nullable Context context) {
         super(context, DATABASE_NAME,null,DATABASE_VERSION);
@@ -30,13 +30,9 @@ public class MyDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE " + TABLE_NAME +
-                        " (" + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        COLUMN_TITLE + " TEXT, " +
-                        COLUMN_AUTHOR + " TEXT, " +
-                        COLUMN_CONDITION + "TEXT, " +
-                        COLUMN_EXCHANGE_FOR + "TEXT0);";
-        db.execSQL(query);
+
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "( " + COLUMN_ID + " integer primary key," + COLUMN_TITLE + " text," +
+                COLUMN_CONDITION + " text," + COLUMN_AUTHOR +" text," + COLUMN_EXCHANGE_FOR +" text" + ");" );
 
     }
 
@@ -47,7 +43,7 @@ public class MyDBHelper extends SQLiteOpenHelper {
 
     }
 
-    void AddBook(String title, String author, String condition, String exchangeFor){
+    void AddBookNearby(String title, String author, String condition, String exchangeFor){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
